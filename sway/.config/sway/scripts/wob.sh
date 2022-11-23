6ed4d92a0ad5027e4885d317f8dfbe013e592cc5
Ary Kleinerman <kleinerman@gmail.com>
2022-11-23 11:51:14 -0300

    Fix and update wob config

--- sway/.config/sway/scripts/wob.sh
+++ sway/.config/sway/scripts/wob.sh
@@ -13,7 +13,7 @@ is_running_on_this_screen() {
     return 1
 }
 
-new_value=$3 # null or a percent; no checking!!
+new_value=$1 # null or a percent; no checking!!
 
 wob_pipe=~/.cache/$( basename $SWAYSOCK ).wob
 
@@ -21,7 +21,7 @@ wob_pipe=~/.cache/$( basename $SWAYSOCK ).wob
 
 # wob does not appear in $(swaymsg -t get_msg), so:
 is_running_on_this_screen wob || {
-    tail -f $wob_pipe | wob --border-color $1 --bar-color $1 --background-color $2 --anchor top --anchor center --margin 20 &
+    tail -f $wob_pipe | wob -c $HOME/.config/wob/config &
 }
 
 [[ "$new_value" ]] && echo $new_value > $wob_pipe
