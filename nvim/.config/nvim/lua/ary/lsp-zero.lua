c7b452bcb05158ce604b8cbd5eb447173de01ae1
Ary Kleinerman <kleinerman@gmail.com>
2023-02-01 15:54:45 -0300

    Update config

--- nvim/.config/nvim/lua/ary/lsp-zero.lua
+++ nvim/.config/nvim/lua/ary/lsp-zero.lua
@@ -2,6 +2,7 @@
 -- Learn to configure LSP servers, see :help lsp-zero-api-showcase
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
+lsp.nvim_workspace()
 lsp.setup()
 
 -- diagnostic config
