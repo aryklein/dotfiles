dd1171a27626e5b047f8c705c44092dda9de1a7c
Ary Kleinerman <kleinerman@gmail.com>
2023-02-02 10:20:49 -0300

    Remove unused func

--- nvim/.config/nvim/lua/ary/lsp-zero.lua
+++ nvim/.config/nvim/lua/ary/lsp-zero.lua
@@ -3,21 +3,18 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
+-- only for lskind-nvim
+-- See https://github.com/VonHeikemen/lsp-zero.nvim/discussions/143#discussioncomment-4845982
+-- block taken from here: https://github.com/onsails/lspkind.nvim#option-2-nvim-cmp
 lsp.setup_nvim_cmp({
-  formatting = {
-    fields = {'abbr', 'kind', 'menu'},
-    format = require('lspkind').cmp_format({
-      mode = 'symbol_text', -- show only symbol annotations
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      before = function (entry, vim_item)
-        return vim_item
-      end
-    })
-  }
+    formatting = {
+        fields = {'abbr', 'kind', 'menu'},
+        format = require('lspkind').cmp_format({
+            mode = 'symbol_text', -- show only symbol annotations
+            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+        })
+    }
 })
 
 lsp.nvim_workspace()
@@ -25,17 +22,17 @@ lsp.setup()
 
 -- diagnostic config
 vim.diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  update_in_insert = false,
-  underline = true,
-  severity_sort = false,
-  float = {
-    focusable = false,
-    style = 'minimal',
-    border = 'rounded',
-    source = 'always',
-    header = '',
-    prefix = '',
-  },
+    virtual_text = true,
+    signs = true,
+    update_in_insert = false,
+    underline = true,
+    severity_sort = false,
+    float = {
+        focusable = false,
+        style = 'minimal',
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+    },
 })
