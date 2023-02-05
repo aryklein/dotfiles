f933d0b1a438443b9f3f99aa675a1d4f22ccc543
Ary Kleinerman <kleinerman@gmail.com>
2023-02-05 17:22:36 -0300

    restructure neovim config

---
+++ nvim/.config/nvim/lua/ary/init.lua
@@ -0,0 +1,32 @@
+-- plugins manged with packer.nvim
+require('ary.plugins')
+
+-- general nvim settings
+require('ary.settings')
+
+-- Telescope
+require('ary.telescope')
+
+-- Treesitter
+require('ary.treesitter')
+
+-- LSP and Autocompletion
+require('ary.lsp-zero')
+
+-- nvim-tree: file explorer
+require('ary.nvim-tree')
+
+-- Autopairs
+--require('ary.autopairs')
+
+-- nvim-web-devicons
+require('ary.nvim-web-devicons')
+
+-- remaps
+require('ary.remaps')
+
+-- filetypes
+require('ary.filetype')
+
+-- Github Copilot
+require('ary.copilot')
