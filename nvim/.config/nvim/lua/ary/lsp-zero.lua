bd220ff0cd4ca20ab21bd1b141617748ce6ec3ba
Ary Kleinerman <kleinerman@gmail.com>
2023-04-25 14:19:27 -0300

    Improve lsp-zero config

--- nvim/.config/nvim/lua/ary/lsp-zero.lua
+++ nvim/.config/nvim/lua/ary/lsp-zero.lua
@@ -27,5 +27,11 @@ cmp.setup({
             maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
             ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
         })
-    }
+    },
+    -- Use Enter key to select completion item
+    -- mapping = {
+    --    ['<CR>'] = cmp.mapping.confirm({select = true})
+    -- },
 })
+
+
