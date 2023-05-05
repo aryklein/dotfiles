0939691a611a8c82df6a93853aab26378bb114ef
Ary Kleinerman <kleinerman@gmail.com>
2023-05-05 15:51:25 -0300

    Rename Neovim plugin

---
+++ nvim/.config/nvim/lua/ary/plugins/NoTrailing.lua
@@ -0,0 +1,7 @@
+return {
+  'aryklein/NoTrailing.nvim',
+  -- dir = "~/code/NoTrailing.nvim",
+  config = true,
+  lazy = true,
+  cmd = {"TrailingTrim", "TrailingHighlightToggle"},
+}
--- nvim/.config/nvim/lua/ary/plugins/trim-whitespace.lua
+++
@@ -1,7 +0,0 @@
-return {
-    'aryklein/trim-whitespace.vim',
-    -- dir = "~/code/trim-whitespace.vim",
-    config = true,
-    lazy = true,
-    cmd = {"WhitespaceTrim", "WhitespaceHighlightToggle"},
-}
