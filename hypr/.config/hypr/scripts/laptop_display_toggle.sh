6a4bb214b42daeb9843dc97e218c8371120b5d31
Ary Kleinerman <kleinerman@gmail.com>
2023-06-10 16:41:29 -0300

    Add laptop display toggle keybind

---
+++ hypr/.config/hypr/scripts/laptop_display_toggle.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Description: Toggles laptop display on/off
+# Author: Ary Kleinerman
+
+# Laptop monitor name
+LAPTOP_MONITOR="eDP-1"
+
+# Check if laptop display is enabled
+hyprctl monitors | grep $LAPTOP_MONITOR > /dev/null 2>&1
+IS_ENABLED=$?
+
+# Toggle laptop display
+if [ $IS_ENABLED -eq 0 ]; then
+    # Disable laptop display
+    hyprctl keyword monitor "eDP-1, disable" > /dev/null 2>&1
+    echo "disabled"
+else
+    # Enable laptop display
+    hyprctl keyword monitor "$(grep monitor=eDP-1 .config/hypr/hyprland.conf | cut -d'=' -f2)" > /dev/null 2>&1
+    echo "enabled"
+fi
