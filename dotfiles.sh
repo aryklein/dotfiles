1e275ecef06b613a9d96e18969bc929a9a86f2e0
Ary Kleinerman <kleinerman@gmail.com>
2023-09-05 17:17:40 -0300

    Force Audacity to use X11

--- dotfiles.sh
+++ dotfiles.sh
@@ -39,7 +39,7 @@ PACKAGES=(
     alacritty bash brave obsproject fontconfig git gtk kitty mako nvim
     ssh sway tfswitch tmux waybar zsh flameshot zoom fzf vscode swaylock
     chromium electron kanshi paru wob swappy backgrounds slack fuzzel spotify
-    pcmanfm tgswitch
+    pcmanfm tgswitch audacity
 )
 
 case "$1" in
