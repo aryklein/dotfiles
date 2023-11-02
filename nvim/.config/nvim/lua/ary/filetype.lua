7d67a1ec645a9a73def94ff40ae609d5a8ec7aaa
Ary Kleinerman <kleinerman@gmail.com>
2023-11-02 11:59:14 -0300

    Change colorcolumn to 120 for yaml

--- nvim/.config/nvim/lua/ary/filetype.lua
+++ nvim/.config/nvim/lua/ary/filetype.lua
@@ -6,7 +6,7 @@ vim.cmd [[
 autocmd BufNewFile,BufRead *.yaml,*.yml
 \ if search('hosts:\|tasks:\|loop:\|tags:', 'nw')
 \ |   set ft=yaml.ansible
-\ |   set colorcolumn=160
+\ |   set colorcolumn=120
 \ | endif
 ]]
 
